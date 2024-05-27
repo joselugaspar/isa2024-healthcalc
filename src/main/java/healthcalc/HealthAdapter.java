c1732351857844abd2243c364bb3fc173ca250e2
José Luis Fernández Gaspar <131762623+joselugaspar@users.noreply.github.com>
2024-05-27 14:26:32 +0200

    Cambios en HealthAdapter para usar la nueva versión de HealthCalcImpl

--- src/main/java/healthcalc/HealthAdapter.java
+++ src/main/java/healthcalc/HealthAdapter.java
@@ -2,20 +2,20 @@ package healthcalc;
 
 public class HealthAdapter implements HealthHospital {
 
-    private HealthCalc healthCalc;
+    private HealthCalcImpl healthCalc;
 
-    public HealthAdapter(HealthCalc healthCalc) {
+    public HealthAdapter(HealthCalcImpl healthCalc) {
         this.healthCalc = healthCalc;
     }
 
     @Override
-    public double bmr(char gender, int age, float height, int weight) {
+    public double bmr(Gender gender, int age, float height, int weight) {
         try {
             // Convertir altura de metros a centímetros
-            int heightInCm = (int) (height * 100);
+            float heightInCm = (int) (height * 100);
             // Convertir peso de gramos a kilogramos
             float weightInKg = weight / 1000.0f;
-            return healthCalc.basalMetabolicRate(weightInKg, heightInCm, gender, age);
+            return healthCalc.basalMetabolicRate(new PersonClass(weightInKg, heightInCm, age, gender));
         } catch (Exception e) {
             e.printStackTrace();
             return 0;
@@ -26,13 +26,23 @@ public class HealthAdapter implements HealthHospital {
     public int idealWeight(char gender, float height) {
         try {
             // Convertir altura de metros a centímetros
-            int heightInCm = (int) (height * 100);
+            float heightInCm = (int) (height * 100);
             // Calcular el peso ideal utilizando la calculadora de salud existente
-            return (int) healthCalc.idealWeight(heightInCm, gender);
+            return (int) healthCalc.getIdealBodyWeight(new PersonClass(heightInCm, getGender(gender)));
         } catch (Exception e) {
             e.printStackTrace();
             return 0;
         }
+    }
+
+    private Gender getGender(char gender){
+        if(gender == 'm'){
+            return Gender.MALE;
+        }
+        if(gender == 'w'){
+            return Gender.FEMALE;
+        }
+        else{throw new RuntimeException("Género no válido.");}
     }
 }
 
