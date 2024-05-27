abdf8cf0cce746260f3335da4c564f53213ea90e
José Luis Fernández Gaspar <131762623+joselugaspar@users.noreply.github.com>
2024-05-27 14:25:51 +0200

    Cambios en HealthCalcImpl para usar las interfaces

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -1,10 +1,13 @@
 package healthcalc;
 
-public class HealthCalcImpl implements HealthCalc {
+public class HealthCalcImpl implements CardioVascularMetrics, MetabolicMetrics {
+
+    private HealthCalcImpl(){
+        super();
+    }
     
     //Implementación de singleton
     private static HealthCalcImpl instance;
-    private HealthCalcImpl() {}
     public static synchronized HealthCalcImpl getInstance() {
         if (instance == null) {
             instance = new HealthCalcImpl();
@@ -13,36 +16,37 @@ public class HealthCalcImpl implements HealthCalc {
     }
 
     @Override
-    public float idealWeight(int height, char gender) throws Exception {
-        if (height <= 0) {
+    public double getIdealBodyWeight(Person person) throws Exception {
+        if (person.height() <= 0) {
             throw new IllegalArgumentException("Height must be a positive value");
         }
 
         float idealWeight = 0.0f;
-        if (gender == 'm') {
-            idealWeight = Math.max(height - 100 - (height - 150) / 4.0f, 0.0f);
-        } else if (gender == 'w') {
-            idealWeight = Math.max(height - 100 - (height - 150) / 2.5f, 0.0f);
+        if (person.gender() == Gender.MALE) {
+            idealWeight = Math.max(person.height() - 100 - (person.height() - 150) / 4.0f, 0.0f);
+        } else if (person.gender() == Gender.FEMALE) {
+            idealWeight = Math.max(person.height() - 100 - (person.height() - 150) / 2.5f, 0.0f);
         } else {
             throw new IllegalArgumentException("Invalid gender provided");
         }
         return idealWeight;
     }
 
+    
     @Override
-    public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception {
-        if (age < 0) {
+    public double basalMetabolicRate(Person person) throws Exception {
+        if (person.age() < 0) {
             throw new IllegalArgumentException("Age must be a non-negative value");
         }
-        if (height < 0){
+        if (person.height() < 0){
             throw new IllegalArgumentException("Height must be a non-negative value");
         }
 
         float bmr = 0.0f;
-        if (gender == 'm') {
-            bmr = 10 * weight + 6.25f * height - 5 * age + 5;
-        } else if (gender == 'w') {
-            bmr = 10 * weight + 6.25f * height - 5 * age - 161;
+        if (person.gender() == Gender.MALE) {
+            bmr = 10 * person.weight() + 6.25f * person.height() - 5 * person.age() + 5;
+        } else if (person.gender() == Gender.FEMALE) {
+            bmr = 10 * person.weight() + 6.25f * person.height() - 5 * person.age() - 161;
         } else {
             throw new IllegalArgumentException("Invalid gender provided");
         }
